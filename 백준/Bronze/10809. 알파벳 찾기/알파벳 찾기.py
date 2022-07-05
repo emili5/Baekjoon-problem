c6404f6921a555c1f2f6da9c37e0eeec983eac7f
Jeongmin An <88500087+emili5@users.noreply.github.com>
2022-07-05 18:40:51 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,7 +1,8 @@
 ch=input()
 for i in range(ord('a'),ord('z')+1):
-    if chr(i) in ch:
-        #문자열 내 찾고 싶은 문자를 매개변수로 보내면 인덱스값 반환
-        print(ch.find(chr(i)),end=" ")
-    else:
-        print(-1, end=" ")
+    for j in range(len(ch)):
+        a=-1 #초기화 위치
+        if chr(i) == ch[j]:
+            a=j
+            break #인덱스 찾았으면 빠져나가기
+    print(a,end=" ")
