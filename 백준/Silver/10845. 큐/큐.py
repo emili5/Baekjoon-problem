20daf4214768a438faee45a9c347d4bdbb5e32d3
Jeongmin An <88500087+emili5@users.noreply.github.com>
2022-07-11 01:14:06 +0900

    [Silver IV] Title: 큐, Time: 184 ms, Memory: 117596 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+n=int(input())
+
+arr=[input() for _ in range(n)]
+stack=[]
+for i in range(n):
+    if arr[i]=='pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[0])
+            stack.remove(stack[0])
+    elif arr[i]=='size':
+        print(len(stack))
+    elif arr[i]=='empty':
+        if len(stack)==0 :
+            print(1)
+        else:
+            print(0)
+    elif arr[i]=='front':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[0])
+    elif arr[i]=='back':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[len(stack)-1])
+    else:
+        k=int(arr[i].split(" ")[1])
+        stack.append(k)
