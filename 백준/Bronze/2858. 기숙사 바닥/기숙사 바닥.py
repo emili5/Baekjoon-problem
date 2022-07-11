21398e70f909030ab5916542f99e679a96b6352e
Jeongmin An <88500087+emili5@users.noreply.github.com>
2022-07-12 01:44:27 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 424 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,9 +1,7 @@
-r,b = map(int,input().split())
-for i in range(3,3000):
-    for j in range(3,3000):
-        if i*2+2*(j-2)==r and (i-2)*(j-2)==b:
-            L,W = i,j
-            break
-if L<W:
-    W,L = L,W
-print(L,W)
+r,b=map(int,input().split())
+
+for L in range(3,r+b+1):
+    W = (r + b) // L
+    if L*W==r+b and (L-2)*(W-2)==b:
+        if L>=W:
+            print(L,W)
